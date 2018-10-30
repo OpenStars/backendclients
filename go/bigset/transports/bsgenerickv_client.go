ed8418759e63becb912278db67ef896e9edd77b4
hieubq <hieubq90@gmail.com>
2018-10-30 15:29:20 +0700

    add get GetIBsGenericClient

--- go/bigset/transports/bsgenerickv_client.go
+++ go/bigset/transports/bsgenerickv_client.go
@@ -2,18 +2,28 @@ package transports
 
 import (
 	"git.apache.org/thrift.git/lib/go/thrift"
-	"github.com/OpenStars/thriftpool"
 	"github.com/OpenStars/backendclients/go/bigset/thrift/gen-go/openstars/core/bigset/generic"
+	"github.com/OpenStars/thriftpool"
 )
 
 var (
 	bsGenericMapPool = thriftpool.NewMapPool(1000, 3600, 3600,
 		thriftpool.GetThriftClientCreatorFunc(func(c thrift.TClient) interface{} { return (generic.NewTStringBigSetKVServiceClient(c)) }),
 		thriftpool.DefaultClose)
+
+	ibsGenericMapPool = thriftpool.NewMapPool(1000, 3600, 3600,
+		thriftpool.GetThriftClientCreatorFunc(func(c thrift.TClient) interface{} { return (generic.NewTIBSDataServiceClient(c)) }),
+		thriftpool.DefaultClose)
 )
 
 //GetBsGenericClient client by host:port
 func GetBsGenericClient(aHost, aPort string) *thriftpool.ThriftSocketClient {
 	client, _ := bsGenericMapPool.Get(aHost, aPort).Get()
+	return client
+}
+
+//GetIBsGenericClient client by host:port
+func GetIBsGenericClient(aHost, aPort string) *thriftpool.ThriftSocketClient {
+	client, _ := ibsGenericMapPool.Get(aHost, aPort).Get()
 	return client
 }
